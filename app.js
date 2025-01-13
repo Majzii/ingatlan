a80efac27d646034e89cee5c4478a8a1d80c40c5
Majzik "Mini Pablo" Bence <majben686@hengersor.hu>
2025-01-13 09:56:20 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 document.getElementById('propertyForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Megakadályozza az oldal újratöltését
+    event.preventDefault();
 
-    // Az űrlap adatainak lekérése
+   
     const title = document.getElementById('propertyTitle').value;
     const description = document.getElementById('propertyDescription').value;
     const image = document.getElementById('propertyImage').value;
 
-    // Új ingatlan kártya létrehozása
+    
     const newCard = document.createElement('div');
     newCard.classList.add('col-12', 'col-sm-6', 'col-md-4', 'col-lg-3', 'mb-4');
     
@@ -21,10 +21,10 @@ document.getElementById('propertyForm').addEventListener('submit', function(even
         </div>
     `;
 
-    // Az új kártya hozzáadása a listához
+   
     document.getElementById('propertyList').appendChild(newCard);
 
-    // A modal bezárása és az űrlap kiürítése
+    
     var myModal = new bootstrap.Modal(document.getElementById('addPropertyModal'));
     myModal.hide();
     document.getElementById('propertyForm').reset();
