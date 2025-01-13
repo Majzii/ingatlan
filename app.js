b6939d486027c742f8c6b26467da15fdadb82b47
Majzik "Mini Pablo" Bence <majben686@hengersor.hu>
2025-01-13 12:17:33 +0100

    update

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ document.getElementById('propertyForm').addEventListener('submit', function(even
 
    
     const title = document.getElementById('propertyTitle').value;
-    const description = document.getElementById('propertyDescription').value;
+    const ar = document.getElementById('propertyAr').value;
+    const meret = document.getElementById('propertyMeret').value;
+    const szoba = document.getElementById('propertySzoba').value;
+    const furdo = document.getElementById('propertyFurdo').value;
     const image = document.getElementById('propertyImage').value;
 
     
@@ -15,7 +18,10 @@ document.getElementById('propertyForm').addEventListener('submit', function(even
             <img src="${image}" class="card-img-top" alt="Új ingatlan">
             <div class="card-body">
                 <h2 class="card-title">${title}</h2>
-                <p class="card-text">${description}</p>
+                <p class="card-text">Ár: ${ar} millió Ft</p>
+                  <p class="card-text">Méret: ${meret} m²</p>
+                  <p class="card-text">Hálószobák száma: ${szoba}</p>
+                  <p class="card-text">Fürdőszobák száma: ${furdo}</p>
                 <a href="mailto:majben686@hengersor.hu?subject=Érdeklődés az ingatlan iránt&body=Érdeklődöm az adott ingatlan iránt." class="btn btn-primary">Érdekel</a>
             </div>
         </div>
